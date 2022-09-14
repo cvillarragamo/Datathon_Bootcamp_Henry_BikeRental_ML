54ec5b390963f136075f475752cdd821ee5393b6
cvillarragamo <67664604+cvillarragamo@users.noreply.github.com>
2022-09-13 21:40:30 -0700

    update read_excel and avg_cnt deleted

--- model_building.py
+++ model_building.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-training=pd.read_excel('bike_train')
+training=pd.read_excel('bike_train.xlsx')
 
 # choose relevant columns 
 training.columns
@@ -12,6 +12,6 @@ model=training[['season','mnth','hr','weathersit', 'temp', 'atemp', 'hum', 'wind
 
 # train test split 
 X = model.drop('cnt', axis =1)
-y = model.avg_cnt.values
+y = model.cnt.values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)

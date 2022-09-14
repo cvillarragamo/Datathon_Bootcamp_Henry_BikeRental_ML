98fa19a9caf1bc4bf76f8ba9c472996efb4fc2bf
cvillarragamo <67664604+cvillarragamo@users.noreply.github.com>
2022-09-13 21:51:56 -0700

    def function

--- model_building.py
+++ model_building.py
@@ -5,13 +5,16 @@ from sklearn.model_selection import train_test_split
 training=pd.read_excel('bike_train.xlsx')
 
 # choose relevant columns 
-training.columns
-model=training[['season','mnth','hr','weathersit', 'temp', 'atemp', 'hum', 'windspeed','cnt']]
+def split_sample():
+    
+    training.columns
+    model=training[['season','mnth','hr','weathersit', 'temp', 'atemp', 'hum', 'windspeed','cnt']]
 
 #the data came with dummies for all categorical features, and some normalization for continuous variables, so we can go to train the model
 
 # train test split 
-X = model.drop('cnt', axis =1)
-y = model.cnt.values
+    X = model.drop('cnt', axis =1)
+    y = model.cnt.values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
